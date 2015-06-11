3e0bbc6c916e99a3fcf8206f79d9681d367a1e1f
Antoine Nguyen <tonio@ngyn.org>
2015-06-11 20:04:36 +0200

    Version 1.0.2.

--- modoboa_stats/modo_extension.py
+++ modoboa_stats/modo_extension.py
@@ -16,7 +16,7 @@ from modoboa.lib import events, parameters
 class Stats(ModoExtension):
     name = "modoboa_stats"
     label = "Statistics"
-    version = "1.0.1"
+    version = "1.0.2"
     description = ugettext_lazy(
         "Graphical statistics about emails traffic using RRDtool"
     )
